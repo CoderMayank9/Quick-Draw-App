5067ea3793dd508966d9cfac004fda5c52a617d9
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-04-27 18:35:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ canvas=createCanvas(300,300);
 canvas.center();
 background("white");
 }
+function preload()
+{
+classifier=ml5.imageClassifier("DoodleNet");
+}
+function clearCanvas()
+{
+background("white");
+}
+function draw()
+{
+strokeWeight(13);
+stroke("black");
+if(mouseIsPressed)
+{
+line(pmouseX,pmouseY,mouseX,mouseY);
+}
+}
